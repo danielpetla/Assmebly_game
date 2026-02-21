83c807b430f6001e880b3b7c757dc4bc3334b9f0
Daniel <daniel.petlaraujo@proton.me>
2026-02-21 15:38:03 +0100

    Static printing completed

--- coursework.s
+++ coursework.s
@@ -3,19 +3,13 @@
 MAP_W: .word 12	# Map width
 MAP_H: .word 8	# Map hight
 
-# 10 = new line, @ = reward, P = player, # = walls
+# @ = reward, P = player, # = walls
 map:
-	.byte '#','#','#','#','#','#','#','#','#','#','#','#',10
-	.byte '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',10
-	.byte '#',' ',' ',' ',' ','P',' ',' ',' ',' ',' ','#',10
-	.byte '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',10
-	.byte '#',' ',' ',' ',' ',' ',' ',' ',' ',' ','@','#',10
-	.byte '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',10
-	.byte '#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',10
-	.byte '#','#','#','#','#','#','#','#','#','#','#','#',10,0
+	.asciiz "############\n#          #\n#    P     #\n#          #\n#         @#\n#          #\n#          #\n############\n"
     
-score: 		.asciiz "Score: 0\n"
-game_over: 	.asciiz "Game Over 0\n"
+score: 		.asciiz "Score: \n"
+game_over: 	.asciiz "Game Over \n"
+new_line:	.asciiz "\n"
     
 x_player: .word 5	# inital row location of the player
 y_player: .word 2	# initial column location of the player
@@ -26,28 +20,34 @@ y_player: .word 2	# initial column location of the player
 # Starting Point -------------------
 
 main:
-	li $sp,0x7fffeffc	# Address of the stock pointer top of the memmory
+	li $sp,0x7fffeffc	# Initialize stack
 	
 	li $t7, 0		# points = 0
 	
-	jal print_score		# Go to print score
+# Print score
+	jal print_score
 	
-	# Move map 2 lines down
+# Move map 2 lines down
 	li $t0,0
 	li $t1,2
-
+	
 print_blank:
-	li $a0,10            # newline
+	la $a0, new_line            # newline
 	jal print
 	addi $t0,$t0,1
 	blt $t0,$t1,print_blank
     
+# Print map
 	jal print_map         # Go print map
+
+# Infinite
 loop:
     j loop			# Infinite loop -> keeps the program running
     
 
-# Printing -------------------------
+# Printing Function ----------------
+
+# $a0 = pointer to null-terminated string
 print:
 	addi $sp, $sp, -8	# Allocate 8 bytes on stack
 	sw   $ra, 4($sp)	# Save return address
@@ -59,13 +59,18 @@ print_loop:
 	lbu  $t0, 0($s0)		# Load byte from string
 	beqz $t0, print_end		# Stop at null terminator
     
-MMIO:
-	li   $t1, 0xffff000c	# MMIO Transmitter Data Register
-	sw   $t0, 0($t1)	# Write character
+wait_ready:
+	li $t1, 0xffff0008	# Transmitter Control Register
+	lw $t2,0($t1)
+	andi $t2,$t2,1		# Check ready bit
+	beqz $t2, wait_ready
 
-	addi $s0, $s0, 1	# Move pointer to next char
-	j print_loop		# Repeat for next character
+	li $t1,0xffff000c	# Transmitter Data Register
+	sw $t0,0($t1)		# Writes char
 
+    addi $s0,$s0,1       # move pointer
+    j print_loop
+   
 print_end:
 	lw   $s0, 0($sp)	# Restore $s0
 	lw   $ra, 4($sp)	# Restore return address
@@ -74,65 +79,26 @@ print_end:
 	
 	
 # Printing map ---------------------
-
-# x = $t0
-# y = $t1
 print_map:
-	addi $sp,$sp,-16	# Allocating 16 bytes of the stack (negative because it grows dowonward)
-	sw $ra,12($sp)		# Saves the return address back to main
-	sw $s0,8($sp)		# Base address of the map
-	sw $s1,4($sp)		# Map width
-	sw $s2,0($sp)		# Reserves for temporary variables
     	
-	la $s0,map		# Loads address of the map into $s0
-	li $s1,13		# Sets map width (including the new line char '10')
-	li $t0,0		# Sets y to start at x = 0
-
-line_loop:
-	li $t1,0		# The y for each new row (always rests to 0 at the start of a new row)
-
-char_loop:
-	mul $t2,$t0,$s1		# Multiplies x * width -> starts the offset for the row.
-	add $t2,$t2,$t1		# Adds offset to the column (t2 points to the current char)
-	add $t2,$s0,$t2 	# Final memmory address of the char
-	lbu $t3,0($t2)		# The actual ASCII char
-	beqz $t3,end_print	# if byte = 0 -> end function
-
-
-# Wating for input -----------------
-	li $t4,0xffff0008	# Address of transmitter control register
+	la $a0,map		# Loads address of the map into $s0
+	jal print		# Calling general print function
 	
-wait_ready:
-	lw $t5,0($t4)		# Read control register	
-	andi $t5,$t5,1		# Check for a ready bit (1 = ready)
-	beqz $t5,wait_ready	# Loop until ready
-
-	li $t4,0xffff000c	# Register that will receive the char
-	sw $t3,0($t4)		# Write char at the terminal
-	addi $t1,$t1,1		# Move to the next column
-	blt $t1,$s1,char_loop	# Loop until column < width
-
-	addi $t0,$t0,1		# Move to the next row
-	blt $t0,8,line_loop	# Loop until row < 8 (nuber of rows)
-
-end_print:
-	lw $s2,0($sp)		# Restores the temp variables 
-	lw $s1,4($sp)		# Restore map width
-	lw $s0,8($sp)		# Restore the base address of the map
-	lw $ra,12($sp)		# Restore return address
-	addi $sp,$sp,16		# Deallocate stack space
-	jr $ra			# Return to caller
+	jr $ra			# Return
 	
-# Printing score and game --------------------
+# Printing score and game over -----
 print_score:
-	addi $sp, $sp, -8	# Allocating memmory	
-	sw $ra,4($sp)		# Saving return address
+	addi $sp, $sp, -4	# Allocating memmory	
+	sw $ra,0($sp)		# Saving return address
 
 	la $a0,score		# $a0 = address of score string
 	jal print		# Call your general print function
+	
+	la $a0, ($t7)
+	jal print
 
-	lw $ra,4($sp)		# Restoring return address
-	addi $sp,$sp,8		# Restor allocated memory
+	lw $ra,0($sp)		# Restoring return address
+	addi $sp,$sp,4		# Restor allocated memory
 	jr $ra			# Jump back to main
 	
 	
